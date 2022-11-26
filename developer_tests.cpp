326fadee2acece86af6a5e8bc941e5811705ba90
Matan Winkler <dambiexs@gmail.com>
2022-11-26 15:04:49 +0200

    Add test check to createAVL_byMerge() test

--- developer_tests.cpp
+++ developer_tests.cpp
@@ -439,6 +439,8 @@ bool createAVL_byMerge()
     //
     AVL_tree<Player*> tree(tree1, tree2, false);
     std::cout << tree.debugging_printTree();
+    std::string finalTree = tree.debugging_printTree();
 
-    return true;
+    std::string wantedTree = "└──4\n    ├──2\n    │   ├──1\n    │   └──3\n    └──6\n        ├──5\n        └──7\n            └──8\n";
+    return treeCompare(finalTree,wantedTree);
 }
