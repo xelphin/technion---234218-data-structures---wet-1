512e5b2967c4afe59a2c0aeffa29934aff17ad70
Matan Winkler <dambiexs@gmail.com>
2022-11-30 14:33:41 +0200

    debug play_match

--- developer_tests.cpp
+++ developer_tests.cpp
@@ -603,12 +603,12 @@ bool worldCup_Play_Match()
     //
     tests += StatusType::SUCCESS == worldCup.play_match(1, 2);
     tests += StatusType::FAILURE == worldCup.play_match(1, 4);
-    //tests += StatusType::FAILURE == worldCup.play_match(1, 5);
+    tests += StatusType::FAILURE == worldCup.play_match(1, 5);
     //worldCup.remove_player(21);
     //tests += StatusType::FAILURE == worldCup.play_match(1, 2);
     //worldCup.add_player(33, 2, 5, 6, 0, true);
     //tests += StatusType::SUCCESS == worldCup.play_match(1, 2);
 
     // TODO: Checked a bit with prints, but difficult to check directly because no public function gives me direct access to Team pointer
-    return tests == 2;
+    return tests == 3;
 }
